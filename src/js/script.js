68a4dd1b85f9209f60d4a0773ccc8c3fc03bfe65
Stanisław Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-05-07 14:47:02 +0200

    Add initActions

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,15 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
+const selectors = {
+  'books': {
+    'template': '#template-book',
+    'bookIMG': '.book__image',
+    'books': '.books-list',
+  }
+};
+
 const addBooks = (books) => {
-  const bookTemplate = Handlebars.compile(document.querySelector('#template-book').innerHTML);
+  const bookTemplate = Handlebars.compile(document.querySelector(selectors.books.template).innerHTML);
   const bookListDOM = document.querySelector('.books-list');
   for (const book of books){
     const bookHTML = bookTemplate(book);
@@ -10,4 +18,23 @@ const addBooks = (books) => {
   }
 };
 
+const  favoriteBooks = [];
+
+const initActions = () => {
+  const domBooks = document.querySelector(selectors.books.books);
+
+  domBooks.addEventListener('dblclick', function(event){
+    event.preventDefault();
+    const thisBook = event.target.parentElement.parentElement;
+    if (thisBook.classList.contains('book__image')) {
+      thisBook.classList.contains('favorite') ? thisBook.classList.remove('favorite') : thisBook.classList.add('favorite');
+      favoriteBooks.includes(thisBook.dataset.id) ? favoriteBooks.splice(favoriteBooks.indexOf(thisBook.dataset.id), 1) : favoriteBooks.push(thisBook.dataset.id);
+      console.log(favoriteBooks);
+    }
+  });
+  domBooks.addEventListener('click', (e)=>{e.preventDefault();});
+
+};
+
 addBooks(dataSource.books);
+initActions();
